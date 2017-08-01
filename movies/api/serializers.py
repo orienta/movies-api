753011471a268c842542377350c9ad211e2c4d22
Heinz Sohm <heinz@orienta.com.co>
2017-08-01 12:56:44 -0500

    cambios

---
+++ movies/api/serializers.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  1 12:48:07 2017
+
+@author: usuario
+"""
+
---
+++ movies/api/urls.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  1 12:47:40 2017
+
+@author: usuario
+"""
+
